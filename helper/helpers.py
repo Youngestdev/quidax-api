94831f377b3ad3d77e2afdd3b40fa78bf628134d
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-07-29 23:46:01 +0100

    Helpers file seperated from main file

---
+++ helper/helpers.py
@@ -0,0 +1,18 @@
+from test import name
+
+
+def user_helper(user):
+    return {
+        "id": user.id,
+        "username": user.username,
+        "full_name": user.fullname,
+        "user_email": user.email,
+    }
+
+
+def check_duplicate(parent, username) -> bool:
+    for uid in parent.keys():
+        _user = user_helper(parent[uid])
+        if _user["username"] == username:
+            raise Exception("Username exists, do use another one.")
+    return False
