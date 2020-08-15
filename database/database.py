aa8f8d07059f577de94f50d911ff28485b8ddd34
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-15 19:30:05 +0100

    Book methods

--- database/database.py
+++ database/database.py
@@ -1,7 +1,9 @@
+from bson import ObjectId
+from pydantic import EmailStr
 from pymongo import MongoClient
 from decouple import config
 
-from helper.helpers import user_helper
+from helper.helpers import user_helper, book_helper
 from helper.responses import success_response
 
 MONGO_DEETS = config('MONGO_DEETS')
@@ -12,41 +14,45 @@ database = client.quidax
 book_collection = database.books
 user_collection = database.users
 
-# book = {
-#   "name": "Dream Psychology",
-#   "author": "Sigmund Freud",
-#   "availability": True,
-#   "label": [
-#     "psychic",
-#     "dreams"
-#   ],
-#   "likes": 100,
-#   "reads": 250,
-#   "year": 1883,
-#   "rating": 4.5,
-#   "genre": "psychology",
-#   "book_cover": "http://myfile.com/img/here"
-# }
-#
-# result = book_collection.insert(book)
-# print('One post ID: {0}'.format(result))
-
 # TODO: Retreive data, post data. Basically CRUD!
 
-def insert_user(user_data):
+def insert_user(user_data: dict) -> dict:
     id = user_collection.insert(user_data)
-    print("New user ID: ", id)
     new_user = user_collection.find_one({"_id": id})
-    return success_response(user_helper(new_user), 200, "User successfully added into the database")
-#
-def retrieve_users():
+    return success_response(book_helper(new_user), 200, "Book successfully added into the database")
+
+def insert_book(book_data: dict) -> dict:
+    id = book_collection.insert(book_data)
+    new_book = book_collection.find_one({"_id": id})
+    return success_response(book_helper(new_book), 200, "User successfully added into the database")
+
+def retrieve_users() -> dict:
     users = list(user_collection.find({}))
     userlist = []
     for user in users:
         userlist.append(user_helper(user))
     return success_response(userlist, 200, "Users retrieved.") if userlist else success_response()
 
-def retrieve_user(id):
+def retrieve_books() -> dict:
+    books = list(book_collection.find({}))
+    booklist = []
+    for book in books:
+        booklist.append(book_helper(book))
+    return success_response(booklist, 200, "Books retrieved.") if booklist else success_response()
+
+def retrieve_user(id: ObjectId) -> dict:
     user = user_collection.find_one({"_id": id})
-    print(user)
     return success_response(user_helper(user), 200, "User Retrieved") if user else success_response()
+
+def retrieve_book(id: ObjectId) -> dict:
+    user = book_collection.find_one({"_id": id})
+    return success_response(book_helper(user), 200, "Book Retrieved") if user else success_response()
+
+def find_user(email: EmailStr) -> bool:
+    user = user_collection.find_one({"email": email})
+    if user:
+        return True
+    return False
+
+def remove_book(id: ObjectId):
+    return book_collection.delete_one({"_id": ObjectId(str(id))})
