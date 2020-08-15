cdefe3fe0c3f6bccd81a6b32fb1d98b25452b943
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-15 19:30:25 +0100

    Small changes to helper functions

--- helper/helpers.py
+++ helper/helpers.py
@@ -1,14 +1,22 @@
-def user_helper(user):
+def user_helper(user) -> dict:
     return {
+        "id": str(user['_id']),
         "username": user['username'],
         "full_name": user['fullname'],
         "user_email": user['email'],
     }
 
-
-def check_duplicate(parent, username) -> bool:
-    for uid in parent:
-        _user = user_helper(uid)
-        if _user["username"] == username:
-            raise Exception("Username exists, do use another one.")
-    return False
+def book_helper(book) -> dict:
+    return {
+        "id": str(book['_id']),
+        "name": book['name'],
+        "author": book['author'],
+        "availability": book['availability'],
+        "label": book['label'],
+        "likes": book['likes'],
+        "reads": book['reads'],
+        "year": book['year'],
+        "rating": book['rating'],
+        "genre": book['genre'],
+        "book_cover": book['book_cover']
+    }
