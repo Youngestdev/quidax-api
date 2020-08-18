1b32e99ec97cbb04d47cfbf70be868dc0e752878
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-19 00:56:22 +0100

    Reformat database file. Removed unnecessary code, simple API shouldn't have complex code.

--- models/Database.py
+++ models/Database.py
@@ -1,15 +1,32 @@
+import json
+from typing import List
+
 from database.database import database
 
+# class Database:
+#     def __init__(self, collection):
+#         self.db = database
+#         self.collection = collection
+#         print("Collection valaue: ", self.collection)
+#
+#     def retrieve(self, helper):
+#         data = self.db[self.collection].find({})
+#
+#         print(data)
+#         print("Data: ", data)
+#         return data
+#
+#     def get(self, id):
+#         data = self.db.collection.find({"_id": id})
+#
+#         return data
+from database.user import retrieve_users
+
+
 class Database:
-    def __init__(self, collection):
-        self.db = database
+    def __init__(self):
+        self.db: List = retrieve_users().get('data')
+        self.users = [user['id'] for user in self.db]
 
-    def retrieve(self, helper):
-        data = list(self.db.collection.find({}))
-        data = [helper(_data) for _data in data]
-        return data
-
-    def get(self, id):
-        data = self.db.collection.find({"_id": id})
-
-        return data
+    def retrieve(self):
+        return self.users
