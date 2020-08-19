116fff044ef4d6d68ccc797e0be9f5289f4b8974
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-19 13:43:32 +0100

    Cleaned file. Removed unnecessary validations works there.

--- auth/jwt_bearer.py
+++ auth/jwt_bearer.py
@@ -1,18 +1,14 @@
 # Code copied from github.com/overrideveloper/HowAreYouApi*
-
-from typing import List
-
-from bson import ObjectId
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-from auth.encode_decode import decodeJWT, validateJWT
+from auth.jwt_handler import decodeJWT
 from models.Database import Database
-from models.model import TokenPayload
 
 
 class JWTBearer(HTTPBearer):
     db: Database = None
+
     def __init__(self, db, auto_error: bool = True):
         super(JWTBearer, self).__init__(auto_error=auto_error)
         self.db = db
@@ -35,17 +31,12 @@ class JWTBearer(HTTPBearer):
             raise HTTPException(status_code=403, detail="Invalid authorization token")
 
     def verify_jwt(self, jwtoken: str) -> bool:
-        payload: TokenPayload = None
         isTokenValid: bool = False
 
         try:
             payload = decodeJWT(jwtoken)
         except:
             payload = None
-
-        if validateJWT(payload):
-            users: List = self.db.retrieve()
-            user_id = str(ObjectId(payload['user_id']))
-            if user_id in users:  # At the moment, this is the best I can do.
-                isTokenValid = True
+        if payload:
+            isTokenValid = True
         return isTokenValid
